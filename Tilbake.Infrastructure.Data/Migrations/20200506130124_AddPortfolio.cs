d2e3dc3ab7f05544494103fadfc57eff9ddb7ee4
Levi Nkata <levi.nkata@outlook.com>
2020-05-07 16:55:28 +0200

    Test with Angular

---
+++ Tilbake.Infrastructure.Data/Migrations/20200506130124_AddPortfolio.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tilbake.Infrastructure.Data.Migrations
+{
+    public partial class AddPortfolio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            if (migrationBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(migrationBuilder));
+            }
+
+            migrationBuilder.CreateTable(
+                name: "Portfolio",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Portfolio", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PortfolioKlient",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PortfolioID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    KlientID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PortfolioKlient", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_PortfolioKlient_Klient_KlientID",
+                        column: x => x.KlientID,
+                        principalTable: "Klient",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PortfolioKlient_Portfolio_PortfolioID",
+                        column: x => x.PortfolioID,
+                        principalTable: "Portfolio",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PortfolioKlient_KlientID",
+                table: "PortfolioKlient",
+                column: "KlientID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PortfolioKlient_PortfolioID",
+                table: "PortfolioKlient",
+                column: "PortfolioID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            if (migrationBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(migrationBuilder));
+            }
+
+            migrationBuilder.DropTable(
+                name: "PortfolioKlient");
+
+            migrationBuilder.DropTable(
+                name: "Portfolio");
+        }
+    }
+}

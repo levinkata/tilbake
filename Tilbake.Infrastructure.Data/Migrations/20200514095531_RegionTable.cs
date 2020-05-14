c07c4c90130d0368996a33c8f3bd497e63da51ff
Levi Nkata <levi.nkata@outlook.com>
2020-05-14 12:09:32 +0200

    Add RegionTable Migration

---
+++ Tilbake.Infrastructure.Data/Migrations/20200514095531_RegionTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tilbake.Infrastructure.Data.Migrations
+{
+    public partial class RegionTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

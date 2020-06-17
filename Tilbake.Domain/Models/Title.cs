14d630438037a2de372dcf5cc592cb72aff9c5b7
levinkata <levi.nkata@outlook.com>
2020-06-17 19:42:51 +0200

    Set ID to Id in Title

--- Tilbake.Domain/Models/Title.cs
+++ Tilbake.Domain/Models/Title.cs
@@ -5,7 +5,7 @@ namespace Tilbake.Domain.Models
 {
     public class Title
     {
-        public Guid ID { get; set; }
+        public Guid Id { get; set; }
 
         [Display(Name = "Title"), Required, StringLength(50)]
         public string Name { get; set; }
--- Tilbake.Infrastructure.Data/Repositories/TitleRepository.cs
+++ Tilbake.Infrastructure.Data/Repositories/TitleRepository.cs
@@ -12,13 +12,6 @@ namespace Tilbake.Infrastructure.Data.Repositories
 {
     public class TitleRepository : BaseRepository, ITitleRepository
     {
-        //private readonly IServiceScopeFactory _serviceScopeFactory;
-
-        //public TitleRepository(IServiceScopeFactory serviceScopeFactory)
-        //{
-        //    _serviceScopeFactory = serviceScopeFactory ?? throw new ArgumentNullException(nameof(serviceScopeFactory));
-        //}
-
         public TitleRepository(TilbakeDbContext context) : base(context) { }
 
         public async Task AddAsync(Title title)
@@ -28,39 +21,17 @@ namespace Tilbake.Infrastructure.Data.Repositories
                 throw new ArgumentNullException(nameof(title));
             }
 
-            //try
-            //{
-            //    using var scope = _serviceScopeFactory.CreateScope();
-            //    var _context = scope.ServiceProvider.GetRequiredService<TilbakeDbContext>();
-
-            //    title.ID = Guid.NewGuid();
-            //    await _context.Titles.AddAsync((Title)title).ConfigureAwait(true);
-            //    return await Task.Run(() => _context.SaveChangesAsync()).ConfigureAwait(true);
-            //}
-            //catch (DbUpdateException ex)
-            //{
-            //    return ex.HResult;
-            //}
-            title.ID = Guid.NewGuid();
+            title.Id = Guid.NewGuid();
             await _context.Titles.AddAsync((Title)title).ConfigureAwait(true);
         }
 
         public void DeleteAsync(Title title)
         {
-            //using var scope = _serviceScopeFactory.CreateScope();
-            //var _context = scope.ServiceProvider.GetRequiredService<TilbakeDbContext>();
-
-            //Title title = await _context.Titles.FindAsync(id).ConfigureAwait(true);
-            //_context.Titles.Remove((Title)title);
-            //return await Task.Run(() => _context.SaveChangesAsync()).ConfigureAwait(true);
             _context.Titles.Remove(title);
         }
 
         public async Task<IEnumerable<Title>> GetAllAsync()
         {
-            //using var scope = _serviceScopeFactory.CreateScope();
-            //var _context = scope.ServiceProvider.GetRequiredService<TilbakeDbContext>();
-
             return await Task.Run(() => _context.Titles
                                                 .OrderBy(n => n.Name)
                                                 .AsNoTracking()
@@ -68,34 +39,12 @@ namespace Tilbake.Infrastructure.Data.Repositories
         }
 
         public async Task<Title> GetAsync(Guid id)
-        {
-            //using var scope = _serviceScopeFactory.CreateScope();
-            //var _context = scope.ServiceProvider.GetRequiredService<TilbakeDbContext>();
-
-            //return await Task.Run(() => _context.Titles
-            //                                    .FirstOrDefaultAsync(e => e.ID == id)).ConfigureAwait(true);
+        {                                   .FirstOrDefaultAsync(e => e.ID == id)).ConfigureAwait(true);
             return await _context.Titles.FindAsync(id).ConfigureAwait(true);
         }
 
         public void UpdateAsync(Title title)
         {
-            //if (title == null)
-            //{
-            //    throw new ArgumentNullException(nameof(title));
-            //}
-
-            //try
-            //{
-            //    using var scope = _serviceScopeFactory.CreateScope();
-            //    var _context = scope.ServiceProvider.GetRequiredService<TilbakeDbContext>();
-
-            //    _context.Titles.Update((Title)title);
-            //    return await Task.Run(() => _context.SaveChangesAsync()).ConfigureAwait(true);
-            //}
-            //catch (DbUpdateException ex)
-            //{
-            //    return ex.HResult;
-            //}
             _context.Titles.Update(title);
         }
     }

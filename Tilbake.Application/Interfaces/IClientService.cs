d800afa6f64b5613ffdc60e2a1866fdd2ce2ec05
levinkata <levi.nkata@outlook.com>
2021-09-01 10:32:32 +0200

    Add AddRange Repository

--- Tilbake.Application/Interfaces/IClientService.cs
+++ Tilbake.Application/Interfaces/IClientService.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Tilbake.Application.Resources;
+using Tilbake.Domain.Enums;
 
 namespace Tilbake.Application.Interfaces
 {
@@ -12,10 +14,14 @@ namespace Tilbake.Application.Interfaces
         Task<ClientResource> GetByIdNumberAsync(string idNumber);
         Task<ClientResource> GetByPolicyIdAsync(Guid policyId);
         Task<IEnumerable<ClientResource>> GetByPortfolioIdAsync(Guid portfolioId);
-        Task<ClientResource> GetByClientId(Guid portfolioId, Guid clientId);
+        Task<ClientResource> GetByClientIdAsync(Guid portfolioId, Guid clientId);
+        Task<int> ImportBulkAsync(Guid portfolioId, Guid fileTemplateId, FileType fileType,
+                            string delimiter, IFormFile uploadFile, int startRow);
         Task<int> AddAsync(ClientSaveResource resource);
+        Task<int> AddRangeAsync(List<ClientSaveResource> resources);
         Task<int> UpdateAsync(ClientResource resource);
         Task<int> DeleteAsync(Guid id);
         Task<int> DeleteAsync(ClientResource resource);
+        Task<int> DeleteRangeAsync(List<ClientResource> resources);
     }
 }

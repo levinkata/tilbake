61857e952f6fac2fe316a10f67f0ee94a94e7634
Levi Nkata <levi.nkata@outlook.com>
2021-09-25 18:56:56 +0200

    Modify RatingMotor Select & Default

--- Tilbake.Application/Resources/RatingMotorSelectResource.cs
+++ Tilbake.Application/Resources/RatingMotorSelectResource.cs
@@ -1,10 +1,12 @@
 using System;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace Tilbake.Application.Resources
 {
     public class RatingMotorSelectResource
     {
+        [Display(Name = "Insurer")]
         public Guid InsurerId { get; set; }
 
         public SelectList InsurerList { get; set; }

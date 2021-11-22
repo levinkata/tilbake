18a1f436e0c9f065c3a6f8001797b0b7f2d276c2
Levi Nkata <levi.nkata@outlook.com>
2021-11-22 19:05:22 +0200

    Removed Startup.cs for .Net 6.0

--- Tilbake.MVC/Program.cs
+++ Tilbake.MVC/Program.cs
@@ -1,24 +1,159 @@
+using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using System;
+using Tilbake.Application.Mapping;
+using Tilbake.Application.Services;
+using Tilbake.Application.Validators;
+using Tilbake.EF.IoC;
+using Tilbake.EF.Persistence.Context;
+using Tilbake.MVC.Areas.Identity;
+using Tilbake.MVC.Extensions;
 
-namespace Tilbake.MVC
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("Starting up");
+
+try
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseSerilog()   //  Added for Serilog by Levi Nkata
-                .UseServiceProviderFactory(new AutofacServiceProviderFactory())  //  Added for Autofac by Levi Nkata
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Console()
+        .ReadFrom.Configuration(ctx.Configuration));
+
+    builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());  //  Added for Autofac by Levi Nkata
+
+    // Register services directly with Autofac here. Don't
+    // call builder.Populate(), that happens in AutofacServiceProviderFactory.
+    builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacModule()));  //  Added for Autofac by Levi Nkata
+
+    // Configure Serilog logging to the console.
+    builder.Logging.AddSerilog();
+
+    // Add services to the container.
+    var connectionString = builder.Configuration.GetConnectionString("Tilbake");
+
+    builder.Services.AddDbContext<TilbakeDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+    builder.Services.AddDbContext<IdentityDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+    builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+            .AddDefaultUI()
+            .AddEntityFrameworkStores<IdentityDbContext>()
+            .AddDefaultTokenProviders();
+
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+    builder.Services.AddTransient<IEmailSender, EmailSender>(i =>
+            new EmailSender(builder.Configuration["EmailSender:Host"],
+                builder.Configuration.GetValue<int>("EmailSender:Port"),
+                builder.Configuration.GetValue<bool>("EmailSender:EnableSSL"),
+                builder.Configuration["EmailSender:Username"],
+                builder.Configuration["EmailSender:Password"]
+                )
+            );
+
+    builder.Services.AddControllersWithViews()
+            .AddFluentValidation(options =>
+            {
+                options.RegisterValidatorsFromAssemblyContaining<BankResourceValidator>();
+                options.DisableDataAnnotationsValidation = true;
+            })
+            .AddRazorRuntimeCompilation();
+
+    builder.Services.AddRazorPages()
+            .AddRazorRuntimeCompilation();
+
+    builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("EmailId", policy =>
+                    policy.RequireClaim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name", "levi.nkata@outlook.com"));
+
+                options.AddPolicy("CreateRole", policy =>
+                    policy.RequireRole("Admin"));
+            });
+
+    builder.Services.AddAutoMapper(typeof(ModelToResourceProfile));
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseMigrationsEndPoint();
     }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseGlobalExceptionMiddleware();     //  Added by Levi Nkata 22/11/2021 for Global Error Handling
+
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    // Write streamlined request completion events, instead of the more verbose ones from the framework.
+    // To use the default framework request logging instead, remove this line and set the "Microsoft"
+    // level in appsettings.json to "Information".
+    app.UseSerilogRequestLogging(); // Added by Levi Nkata 22/11/2021
+
+    app.UseRouting();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.MapRazorPages();
+
+    app.Run();
+
+
 }
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
+
+//namespace Tilbake.MVC
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            CreateHostBuilder(args).Build().Run();
+//        }
+
+//        public static IHostBuilder CreateHostBuilder(string[] args) =>
+//            Host.CreateDefaultBuilder(args)
+//                .UseSerilog()   //  Added for Serilog by Levi Nkata
+//                .UseServiceProviderFactory(new AutofacServiceProviderFactory())  //  Added for Autofac by Levi Nkata
+//                .ConfigureWebHostDefaults(webBuilder =>
+//                {
+//                    webBuilder.UseStartup<Startup>();
+//                });
+//    }
+//}

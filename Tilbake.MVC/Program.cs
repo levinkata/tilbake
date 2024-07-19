7b4a951e3d73ed09fa1346e6fa33ee63c8a933fe
Levi Nkata <levi.nkata@outlook.com>
2024-07-19 23:15:47 +0200

    Remove Serilog Configuration

--- Tilbake.MVC/Program.cs
+++ Tilbake.MVC/Program.cs
@@ -9,7 +9,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-// using Serilog;
 using System;
 using Tilbake.Core.Context;
 using Tilbake.EF.IoC;
@@ -20,119 +19,96 @@ using Tilbake.MVC.Mapping;
 using Tilbake.MVC.Services;
 using Tilbake.MVC.Validators;
 
-// Log.Logger = new LoggerConfiguration()
+var builder = WebApplication.CreateBuilder(args);
+
+// builder.Host.UseSerilog((ctx, lc) => lc
 //     .WriteTo.Console()
-//     .CreateBootstrapLogger();
+//     .ReadFrom.Configuration(ctx.Configuration));
 
-// Log.Information("Starting up");
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());  //  Added for Autofac by Levi Nkata
 
-// try
-// {
-    var builder = WebApplication.CreateBuilder(args);
+// Register services directly with Autofac here. Don't
+// call builder.Populate(), that happens in AutofacServiceProviderFactory.
+builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacModule()));  //  Added for Autofac by Levi Nkata
 
-    // builder.Host.UseSerilog((ctx, lc) => lc
-    //     .WriteTo.Console()
-    //     .ReadFrom.Configuration(ctx.Configuration));
+// Configure Serilog logging to the console.
+// builder.Logging.AddSerilog();
 
-    builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());  //  Added for Autofac by Levi Nkata
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("Tilbake");
 
-    // Register services directly with Autofac here. Don't
-    // call builder.Populate(), that happens in AutofacServiceProviderFactory.
-    builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacModule()));  //  Added for Autofac by Levi Nkata
+builder.Services.AddDbContext<TilbakeDbContext>(options =>
+        options.UseSqlServer(connectionString));
 
-    // Configure Serilog logging to the console.
-    // builder.Logging.AddSerilog();
+builder.Services.AddDbContext<IdentityDbContext>(options =>
+        options.UseSqlServer(connectionString));
 
-    // Add services to the container.
-    var connectionString = builder.Configuration.GetConnectionString("Tilbake");
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+        .AddDefaultUI()
+        .AddEntityFrameworkStores<IdentityDbContext>()
+        .AddDefaultTokenProviders();
 
-    builder.Services.AddDbContext<TilbakeDbContext>(options =>
-            options.UseSqlServer(connectionString));
+builder.Services.AddScoped<IUserClaimsPrincipalFactory<ApplicationUser>, ApplicationUserClaimsPrincipalFactory>();
 
-    builder.Services.AddDbContext<IdentityDbContext>(options =>
-            options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-    builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
-            .AddDefaultUI()
-            .AddEntityFrameworkStores<IdentityDbContext>()
-            .AddDefaultTokenProviders();
+builder.Services.AddTransient<IEmailSender, EmailSender>(i =>
+        new EmailSender(builder.Configuration["EmailSender:Host"],
+            builder.Configuration.GetValue<int>("EmailSender:Port"),
+            builder.Configuration.GetValue<bool>("EmailSender:EnableSSL"),
+            builder.Configuration["EmailSender:Username"],
+            builder.Configuration["EmailSender:Password"]
+            )
+        );
 
-    builder.Services.AddScoped<IUserClaimsPrincipalFactory<ApplicationUser>, ApplicationUserClaimsPrincipalFactory>();
+builder.Services.AddControllersWithViews()
+        .AddFluentValidation(options =>
+        {
+            options.RegisterValidatorsFromAssemblyContaining<BankValidator>();
+            options.DisableDataAnnotationsValidation = true;
+        });
 
-    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddRazorPages();
 
-    builder.Services.AddTransient<IEmailSender, EmailSender>(i =>
-            new EmailSender(builder.Configuration["EmailSender:Host"],
-                builder.Configuration.GetValue<int>("EmailSender:Port"),
-                builder.Configuration.GetValue<bool>("EmailSender:EnableSSL"),
-                builder.Configuration["EmailSender:Username"],
-                builder.Configuration["EmailSender:Password"]
-                )
-            );
+builder.Services.AddAuthorization(options =>
+        {
+            options.AddPolicy("EmailId", policy =>
+                policy.RequireClaim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name", "levi.nkata@outlook.com"));
+            
+            options.AddPolicy("CreateRole", policy =>
+                policy.RequireRole("Admin"));
+        });
 
-    builder.Services.AddControllersWithViews()
-            .AddFluentValidation(options =>
-            {
-                options.RegisterValidatorsFromAssemblyContaining<BankValidator>();
-                options.DisableDataAnnotationsValidation = true;
-            });
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 
-    builder.Services.AddRazorPages();
+var app = builder.Build();
 
-    builder.Services.AddAuthorization(options =>
-            {
-                options.AddPolicy("EmailId", policy =>
-                    policy.RequireClaim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name", "levi.nkata@outlook.com"));
-                
-                options.AddPolicy("CreateRole", policy =>
-                    policy.RequireRole("Admin"));
-            });
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseGlobalExceptionMiddleware();     //  Added by Levi Nkata 22/11/2021 for Global Error Handling
 
-    builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
-    var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseMigrationsEndPoint();
-    }
-    else
-    {
-        app.UseExceptionHandler("/Home/Error");
-        app.UseGlobalExceptionMiddleware();     //  Added by Levi Nkata 22/11/2021 for Global Error Handling
+app.UseRouting();
 
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-    }
+app.UseAuthentication();
+app.UseAuthorization();
 
-    app.UseHttpsRedirection();
-    app.UseStaticFiles();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-    // Write streamlined request completion events, instead of the more verbose ones from the framework.
-    // To use the default framework request logging instead, remove this line and set the "Microsoft"
-    // level in appsettings.json to "Information".
-    // app.UseSerilogRequestLogging(); // Added by Levi Nkata 22/11/2021
+app.MapRazorPages();
 
-    app.UseRouting();
-
-    app.UseAuthentication();
-    app.UseAuthorization();
-
-    app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
-
-    app.MapRazorPages();
-
-    app.Run();
-// }
-// catch (Exception ex)
-// {
-//     Log.Fatal(ex, "Unhandled exception");
-// }
-// finally
-// {
-//     Log.Information("Shut down complete");
-//     Log.CloseAndFlush();
-// }
+app.Run();
